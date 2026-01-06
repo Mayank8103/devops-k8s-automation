55fcf9252db512610b9cf2a60eedeea5e6d12003
Devang363 <devangjadoun62@gamil.com>
2026-01-07 03:24:08 +0530

    Add application, Kubernetes manifests, and Terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "devops-gke"
+  location = var.region
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 30
+  }
+}
